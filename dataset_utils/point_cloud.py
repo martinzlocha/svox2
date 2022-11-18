b7fe49f9913576dbcaa83fc600e63d32a17928e6
mackopes <pialamar@gmail.com>
2022-11-18 14:49:21 +0000

    make pointcloud loader faster

--- dataset_utils/point_cloud.py
+++ dataset_utils/point_cloud.py
@@ -1,15 +1,16 @@
 from dataclasses import dataclass
-from functools import lru_cache
+from functools import lru_cache, partial
 import json
 import os
-from typing import List, Optional, TypeVar
+from typing import Dict, List, Optional, TypeVar
 import numpy as np
 import torch
 import imageio
 import cv2
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
-from constants import DEPTH_DIR, IMAGE_DIR, ORIGINAL_SUFFIX, TRANSFORMS
+from constants import DEPTH_DIR, IMAGE_DIR
 
 @dataclass
 class Rays:
@@ -74,6 +75,36 @@ def _depth_file_path_from_frame(frame: dict, depth_dir: str, dataset_dir: str) -
 
     return os.path.join(depth_dir, f"{frame['image_id']:04d}.exr")
 
+def _get_points_and_features(frame: Dict, dataset_path: str, images_dir: str, depths_dir: str, camera_angle_x: float, clipping_distance: Optional[float] = None):
+    img_name = frame["file_path"]
+    # img_path = os.path.join(images_dir, f"{img_name}.jpg")
+    img_path = _img_file_path_from_frame(frame, images_dir, dataset_path)
+    img = imageio.imread(img_path)
+    height, width, _ = img.shape
+
+    # depth_path = os.path.join(depths_dir, f"{img_name}.exr")
+    depth_path = _depth_file_path_from_frame(frame, depths_dir, dataset_path)
+    depth = load_depth_file(depth_path)
+    depth_height, depth_width = depth.shape
+    depth = depth.reshape(-1, 1)
+    if clipping_distance is not None:
+        depth = torch.clip(depth, 0, clipping_distance)
+
+    focal = float(0.5 * depth_width / np.tan(0.5 * camera_angle_x))
+    rays = get_rays(torch.tensor(frame["transform_matrix"]), depth_width, depth_height, focal)
+
+    img_points = rays.origins + rays.dirs * depth
+    # points_list.append(img_points)
+
+    img = cv2.resize(img, (depth_width, depth_height), interpolation=cv2.INTER_CUBIC)
+    img = torch.from_numpy(img).reshape(-1, 3)
+
+    assert img.shape[0] == img_points.shape[0], f"img.shape: {img.shape}, img_points.shape: {img_points.shape}"
+
+    # features_list.append(img)
+
+    return img_points, img
+
 
 class Pointcloud:
     def __init__(self, points: torch.Tensor, features: Optional[torch.Tensor]=None):
@@ -96,33 +127,17 @@ class Pointcloud:
 
             camera_angle_x = transforms["camera_angle_x"]
 
-            for i, frame in enumerate(tqdm(transforms["frames"], desc=f"Loading {transforms_name} pointcloud")):
-                img_name = frame["file_path"]
-                # img_path = os.path.join(images_dir, f"{img_name}.jpg")
-                img_path = _img_file_path_from_frame(frame, images_dir, dataset_path)
-                img = imageio.imread(img_path)
-                height, width, _ = img.shape
+            with ThreadPoolExecutor() as executor:
+                points_features = list(tqdm(executor.map(partial(_get_points_and_features, dataset_path=dataset_path, images_dir=images_dir, depths_dir=depths_dir, camera_angle_x=camera_angle_x, clipping_distance=clipping_distance), transforms["frames"]), total=len(transforms["frames"])))
 
-                # depth_path = os.path.join(depths_dir, f"{img_name}.exr")
-                depth_path = _depth_file_path_from_frame(frame, depths_dir, dataset_path)
-                depth = load_depth_file(depth_path)
-                depth_height, depth_width = depth.shape
-                depth = depth.reshape(-1, 1)
-                if clipping_distance is not None:
-                    depth = torch.clip(depth, 0, clipping_distance)
+            points, features = zip(*points_features)
+            points_list.extend(points)
+            features_list.extend(features)
 
-                focal = float(0.5 * depth_width / np.tan(0.5 * camera_angle_x))
-                rays = get_rays(torch.tensor(frame["transform_matrix"]), depth_width, depth_height, focal)
-
-                img_points = rays.origins + rays.dirs * depth
-                points_list.append(img_points)
-
-                img = cv2.resize(img, (depth_width, depth_height), interpolation=cv2.INTER_CUBIC)
-                img = torch.from_numpy(img).reshape(-1, 3)
-
-                assert img.shape[0] == img_points.shape[0], f"img.shape: {img.shape}, img_points.shape: {img_points.shape}"
-
-                features_list.append(img)
+            # for i, frame in enumerate(tqdm(transforms["frames"], desc=f"Loading {transforms_name} pointcloud")):
+            #     points, features = _get_points_and_features(frame, dataset_path, images_dir, depths_dir, camera_angle_x, clipping_distance)
+            #     points_list.append(points)
+            #     features_list.append(features)
 
 
         points = torch.cat(points_list, dim=0)
