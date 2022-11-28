3dbf991e671fb9c8c325f69c1ae519eaaa4d9760
Michal Pandy <mpmisko@gmail.com>
2022-11-28 23:34:52 +0000

    loop registration

--- dataset_utils/pointcloud_registration.py
+++ dataset_utils/pointcloud_registration.py
@@ -1,72 +1,27 @@
-import copy
-import json
-import os
-from concurrent.futures import ThreadPoolExecutor
-from typing import Dict, List
-
-import cv2
-import imageio.v2 as imageio
-import numpy as np
 import open3d as o3d
-from fire import Fire
-from tqdm import tqdm
-from utils import (depth_file_path_from_frame,
-                                 img_file_path_from_frame, load_depth_file)
-from point_cloud import Pointcloud, stack_pointclouds
-import time
-
-
 if o3d.__DEVICE_API__ == 'cuda':
     import open3d.cuda.pybind.t.pipelines.registration as treg
     device = o3d.core.Device("CUDA:0")
 else:
     import open3d.cpu.pybind.t.pipelines.registration as treg
     device = o3d.core.Device("CPU:0")
+import copy
+import json
+import os
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict, List
+
+import imageio.v2 as imageio
+import numpy as np
+from fire import Fire
+from tqdm import tqdm
+from utils import (depth_file_path_from_frame,
+                                 img_file_path_from_frame, load_depth_file)
+from point_cloud import Pointcloud
+
 
 
 class FrameData:
-    # class Matching:
-    #     def __init__(self, rgb: np.ndarray):
-    #         # detector = cv2.xfeatures2d.SIFT_create()
-    #         # self.keypoints, self.descriptors = detector.detectAndCompute(rgb, None)
-    #         self.hist = self.get_hist(rgb)
-
-    #     @staticmethod
-    #     def get_hist(img):
-    #         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #         h_bins = 50
-    #         s_bins = 60
-    #         histSize = [h_bins, s_bins]
-
-    #         # hue varies from 0 to 179, saturation from 0 to 255
-    #         h_ranges = [0, 180]
-    #         s_ranges = [0, 256]
-    #         ranges = h_ranges + s_ranges  # concat lists
-    #         channels = [0, 1]
-
-    #         hist = cv2.calcHist([hsv], channels, None, histSize, ranges, accumulate=False)
-    #         cv2.normalize(hist, hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-
-    #         return hist
-
-    #     def similarity_score(self, other: "FrameData.Matching") -> float:
-    #         # matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
-    #         # knn_matches = matcher.knnMatch(self.descriptors, other.descriptors, 2)
-
-    #         # ratio_thresh = 0.7
-    #         # good_matches = []
-    #         # for m,n in knn_matches:
-    #         #     if m.distance < ratio_thresh * n.distance:
-    #         #         good_matches.append(m)
-
-    #         # return len(good_matches) / len(self.descriptors)
-
-    #         compare_method = cv2.HISTCMP_BHATTACHARYYA
-    #         res = cv2.compareHist(self.hist, other.hist, compare_method)
-
-    #         return 1 - res
-
-
     def __init__(self, frame_data: Dict, dataset_dir: str, camera_angle_x: float):
         self.camera_angle_x = camera_angle_x
 
@@ -87,12 +42,6 @@ class FrameData:
         """
         self.transform_matrix = transform_matrix @ self.transform_matrix
         self.pointcloud.transform_(transform_matrix)
-
-    # @property
-    # def matching(self) -> Matching:
-    #     if self._matching is None:
-    #         self._matching = FrameData.Matching(self.rgb)
-    #     return self._matching
 
     def similarity_score(self, other: "FrameData") -> float:
         # TODO: we should use covisibility matrix instead
@@ -116,7 +65,8 @@ def draw_registration_result(source, target, transformation):
                                       up=[-0.3402, -0.9189, -0.1996])
 
 
-def load_frame_data_from_dataset(dataset_dir: str, transforms_json_file: str) -> List[FrameData]:
+def load_frame_data_from_dataset(dataset_dir: str,
+                                 transforms_json_file: str) -> List[FrameData]:
     with open(os.path.join(dataset_dir, transforms_json_file), "r") as f:
         transforms = json.load(f)
 
@@ -201,71 +151,24 @@ def get_n_closest_frames(frame_data: List[FrameData], frame: FrameData, n: int) 
     return [frame_data[i] for i in closest_indices]
 
 
-def main(dataset_dir: str):
+def run_pairwise_icp(dataset_dir: str):
     transforms_train = os.path.join(dataset_dir, 'transforms_train_original.json')
-    # transforms_test = os.path.join(dataset_dir, 'transforms_test.json')
-
     with open(transforms_train, 'r') as f:
         train_json = json.load(f)
-    image_ids = [frame['image_id'] for frame in train_json['frames']]
-    n_images = len(image_ids)
-    # point_cloud_train = Pointcloud.from_dataset(dataset_dir, [transforms_train])
-
-    # print(point_cloud_train._n_frames)
 
     frame_data = load_frame_data_from_dataset(dataset_dir, transforms_train)
-
-    registered_frames = []
-
+    frame = frame_data[0]
+    registered_frames = [frame]
     rolling_init_transform = np.eye(4)
-
-    big_boy_pointcloud = None
+    big_boy_pointcloud = frame.pointcloud.as_open3d_tensor()
+    big_boy_pointcloud.estimate_normals(max_nn=30, radius=0.2)
 
     for i, frame in enumerate(tqdm(frame_data)):
-        if len(registered_frames) == 0:
-            registered_frames.append(frame)
-            big_boy_pointcloud = frame.pointcloud.as_open3d_tensor()
-            big_boy_pointcloud.estimate_normals(max_nn=30, radius=0.2)
-            continue
-
-        tic = time.time()
-        # closest_frames = get_n_closest_frames(registered_frames, frame, 5)
-
-        # print(f"Closest frames found in {toc - tic:.2f}s")
-        # tic = time.time()
-        # merged_pointcloud = stack_pointclouds([f.pointcloud for f in closest_frames])
-
-        assert big_boy_pointcloud is not None
-
         frame_pcd = frame.pointcloud.as_open3d_tensor().transform(rolling_init_transform)
         bounding_box = frame_pcd.get_axis_aligned_bounding_box()
         bounding_box.scale(2, center=bounding_box.get_center())
         merged_pointcloud = big_boy_pointcloud.crop(bounding_box)
-        # merged_pointcloud.estimate_normals(max_nn=30, radius=0.15)
-
-        # toc = time.time()
-        # print(f"Pointclouds stacked in {toc - tic:.2f}s")
-
-        # print("merged pointcloud has", merged_pointcloud._points.shape[0], "points")
-
-        # reg_p2p = o3d.pipelines.registration.registration_icp(
-        #     frame_pcd, merged_pointcloud, 1e-2, np.eye(4),
-        #     o3d.pipelines.registration.TransformationEstimationPointToPoint(),
-        #     o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200))
-
-        max_correspondence_distance = 0.25
-
-        # Initial alignment or source to target transform.
-
-        # Select the `Estimation Method`, and `Robust Kernel` (for outlier-rejection).
-        # estimation = treg.TransformationEstimationPointToPoint()
         estimation = treg.TransformationEstimationPointToPlane()
-
-        # Convergence-Criteria for Vanilla ICP
-        criteria = treg.ICPConvergenceCriteria(relative_fitness=0.000001,
-                                            relative_rmse=0.000001,
-                                            max_iteration=50)
-
         voxel_sizes = o3d.utility.DoubleVector([0.25, 0.15, 0.03, 0.008, 0.002])
 
         criteria_list = [
@@ -278,73 +181,112 @@ def main(dataset_dir: str):
             treg.ICPConvergenceCriteria(0.000001, 0.000001, 10)
         ]
 
-        max_correspondence_distances = o3d.utility.DoubleVector([0.6, 0.4, 0.09, 0.04, 0.01])
-
-        # Run the ICP algorithm.
-        # Down-sampling voxel-size.
-        voxel_size = 0.025
-
-        tic = time.time()
-        # frame_pcd = frame.pointcloud.as_open3d_tensor()
-        # merged_pcd = merged_pointcloud.as_open3d_tensor()
+        max_correspondence_distances = o3d.utility.DoubleVector([1.0, 0.4, 0.09, 0.04, 0.01])
         merged_pcd = merged_pointcloud
-        toc = time.time()
-        # print(f"Pointclouds converted to Open3D in {toc - tic:.2f}s")
+        registration_icp = treg.multi_scale_icp(frame_pcd,
+                                  merged_pcd,
+                                  voxel_sizes,
+                                  criteria_list,
+                                  max_correspondence_distances,
+                                  np.eye(4),
+                                  estimation)
 
-        # registration_icp = treg.icp(frame.pointcloud.as_open3d_tensor(), merged_pointcloud.as_open3d_tensor(), max_correspondence_distance,
-        #                     rolling_init_transform, estimation, criteria, voxel_size)
-
-        tic = time.time()
-        registration_icp = treg.multi_scale_icp(frame_pcd, merged_pcd, voxel_sizes,
-                                           criteria_list,
-                                           max_correspondence_distances,
-                                           np.eye(4), estimation)
-
-        toc = time.time()
-        # print(f"ICP done in {toc - tic:.2f}s")
-
-
-        # print("Transformation is:")
-        # print(reg_p2p.transformation)
-        # draw_registration_result(frame_pcd, overall_point_cloud, reg_p2p.transformation)
-
-        # print(transformation.rot)
-        # print(transformation.t)
 
         np_transform = registration_icp.transformation.numpy()
         rolling_init_transform = np_transform @ rolling_init_transform
-
-        # frame_transforms.append(np_transform)
-        # camera.transform(np_transform)
         frame.transform_(np_transform)
-
-        if np.max(np_transform[:3, 3])>0.1:
-            print(np_transform)
 
         if i % 20 == 0:
             registered_frames.append(frame)
             p = frame_pcd.transform(registration_icp.transformation)
             p.estimate_normals(max_nn=30, radius=0.2)
-
             big_boy_pointcloud += p
-
-        # frame_pcd.points = transformation.transform(frame_pcd.points)
-
-        # if i % 200 == 0:
-        #     big_boy_pointcloud.estimate_normals(max_nn=30, radius=0.2)
-
 
     transforms_train_shifted = os.path.join(dataset_dir, 'transforms_train_original_shifted.json')
 
     with open(transforms_train_shifted, 'w') as f:
         new_frames = []
         for frame, registered_frame in zip(train_json['frames'], registered_frames):
-            # matrix = additional_transform @ np.asarray(frame['transform_matrix'])
             matrix = registered_frame.transform_matrix
             frame['transform_matrix'] = matrix.tolist()
             new_frames.append(frame)
         json.dump(train_json, f, indent=4)
 
+def pairwise_registration(source, target, trans_init, max_correspondence_distance=0.1):
+    registration_icp = treg.icp(
+                source,
+                target,
+                max_correspondence_distance=max_correspondence_distance,
+                init_source_to_target=trans_init,
+                estimation_method=treg.TransformationEstimationPointToPoint(),
+                criteria=treg.ICPConvergenceCriteria(relative_fitness=0.0001,
+                                        relative_rmse=0.0001,
+                                        max_iteration=100),
+                voxel_size=0.4)
+    transformation_icp = registration_icp.transformation
+    information_icp = treg.get_information_matrix(source,
+                                                  target,
+                                                  max_correspondence_distance,
+                                                  transformation_icp)
+    return transformation_icp, information_icp
+
+
+def invert_transformation_matrix(matrix):
+    R = matrix[:3, :3]
+    t = matrix[:3, 3:]
+    inverted_matrix = np.concatenate([R.T, -R.T @ t], axis=-1)
+    inverted_matrix = np.concatenate([inverted_matrix,
+                                 np.array([[0., 0., 0., 1.]])],
+                                 axis=0)
+    return inverted_matrix
+
+
+def run_full_icp(dataset_dir: str):
+    transforms_train = os.path.join(dataset_dir,
+                                    'transforms_train_original.json')
+    with open(transforms_train, 'r') as f:
+        train_json = json.load(f)
+
+    frame_data = load_frame_data_from_dataset(dataset_dir, transforms_train)
+    pcds = frame_data
+    pose_graph = o3d.pipelines.registration.PoseGraph()
+    odometry = o3d.core.Tensor(np.identity(4))
+    pose_graph.nodes.append(o3d.pipelines.registration.PoseGraphNode(odometry))
+    n_pcds = len(pcds)
+    for source_id in range(n_pcds):
+        source_pcd = pcds[source_id].pointcloud.as_open3d_tensor().transform(odometry)
+        source_trans_inv = invert_transformation_matrix(pcds[source_id].transform_matrix)
+        for target_id in range(source_id + 1, n_pcds):
+            target_pcd = pcds[target_id].pointcloud.as_open3d_tensor().transform(odometry)
+            target_trans = pcds[target_id].transform_matrix
+            trans_init = target_trans @ source_trans_inv
+            transformation_icp, information_icp = pairwise_registration(source_pcd,
+                                                                        target_pcd,
+                                                                        o3d.core.Tensor(trans_init))
+
+            if target_id == source_id + 1:  # odometry case
+                odometry = o3d.core.matmul(transformation_icp, odometry)
+                pose_graph.nodes.append(
+                    o3d.pipelines.registration.PoseGraphNode(
+                        o3d.core.inv(odometry)))
+                pose_graph.edges.append(
+                    o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                             target_id,
+                                                             transformation_icp,
+                                                             information_icp,
+                                                             uncertain=False))
+            else:  # loop closure case
+                pose_graph.edges.append(
+                    o3d.pipelines.registration.PoseGraphEdge(source_id,
+                                                             target_id,
+                                                             transformation_icp,
+                                                             information_icp,
+                                                             uncertain=True))
+    return pose_graph
+
+
+def main(dataset_dir: str):
+    run_full_icp(dataset_dir)
 
 if __name__ == "__main__":
     Fire(main)
